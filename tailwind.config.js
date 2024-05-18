7d7aae137de8be791a067627aab0c6b6012af356
SANJAYKUMAR K <117352009+SANJAYKUMAR-GITHUB@users.noreply.github.com>
2024-05-18 23:19:04 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}','./src/app/search/search.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      margin: {
+      '250': '250px',
+    },
+    width :{
+      '10':'10rem',
+    },
+    height:{
+      '10': '10rem',
+    }
+  },
   },
   plugins: [],
 }
